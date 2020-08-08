fa5626dd0e046f49220b02c1806d6e55217b9318
drzhang <654014730@qq.com>
2020-08-08 22:47:08 +0800

    rabbitmq test

--- go.mod
+++ go.mod
@@ -11,7 +11,7 @@ require (
 	github.com/golang/protobuf v1.4.2 // indirect
 	github.com/google/uuid v1.1.1 // indirect
 	github.com/nsqio/go-nsq v1.0.8
-	github.com/streadway/amqp v1.0.0 // indirect
+	github.com/streadway/amqp v1.0.0
 	go.uber.org/zap v1.15.0 // indirect
 	google.golang.org/genproto v0.0.0-20200731012542-8145dea6a485 // indirect
 	google.golang.org/grpc v1.31.0 // indirect
--- logAgent/server/rabbitmq_test.go
+++ logAgent/server/rabbitmq_test.go
@@ -1,7 +1,80 @@
 package server
 
-import "testing"
+import (
+	"github.com/streadway/amqp"
+	"log"
+	"testing"
+	"time"
+)
 
 func TestProducer(t *testing.T) {
-	
+	conn, err := amqp.Dial("amqp://admin:admin@47.103.9.218:5672/")
+	if err != nil {
+		log.Panic(err)
+	}
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	q, err := ch.QueueDeclare(
+		"testqueue",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	err = ch.Publish("", q.Name, false, false, amqp.Publishing{
+		ContentType:  "text/plain",
+		DeliveryMode: amqp.Persistent,
+		Body:         []byte("this is golang to rabbitmq"),
+	})
+	if err != nil {
+		log.Panic(err)
+	}
+
+}
+
+func TestConsumer(t *testing.T) {
+	conn, err := amqp.Dial("amqp://admin:admin@47.103.9.218:5672/")
+	if err != nil {
+		log.Panic(err)
+	}
+	defer conn.Close()
+
+	ch := make(chan bool)
+	for i := 0; i < 4; i++ {
+		go func(rt int) {
+			ch, err := conn.Channel()
+			if err != nil {
+				log.Panic(err)
+			}
+			defer ch.Close()
+
+			q, err := ch.QueueDeclare("testqueue", true, false, false, false, nil)
+			if err != nil {
+				log.Panic(err)
+			}
+			defer ch.Close()
+
+			msgs, err := ch.Consume(q.Name, "", false, false, false, false, nil)
+			if err != nil {
+				log.Panic(err)
+			}
+
+			for msg := range msgs {
+				log.Printf("协程 %d    Received a message: %s", rt, msg.Body)
+				time.Sleep(5 * time.Second)
+				msg.Ack(true)
+			}
+		}(i)
+	}
+	<-ch
 }
